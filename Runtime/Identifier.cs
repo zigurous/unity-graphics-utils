4c332f7a8b5fbd58e9406f7f9935780338db4313
Adam Graham <adam@zigurous.com>
2021-07-01 07:13:46 -0500

    Add new shader property identifiers

--- Runtime/Identifier.cs
+++ Runtime/Identifier.cs
@@ -2,16 +2,46 @@
 
 namespace Zigurous.Graphics
 {
+    /// <summary>
+    /// Constant shader property identifiers.
+    /// </summary>
     public static class Identifier
     {
+        /// <summary>Shader.PropertyToID("_BumpMap")</summary>
+        public static readonly int BumpMap = Shader.PropertyToID("_BumpMap");
+
+        /// <summary>Shader.PropertyToID("_BumpScale")</summary>
+        public static readonly int BumpScale = Shader.PropertyToID("_BumpScale");
+
         /// <summary>Shader.PropertyToID("_Color")</summary>
         public static readonly int Color = Shader.PropertyToID("_Color");
 
         /// <summary>Shader.PropertyToID("_Cubemap")</summary>
         public static readonly int Cubemap = Shader.PropertyToID("_Cubemap");
 
+        /// <summary>Shader.PropertyToID("_Cutoff")</summary>
+        public static readonly int Cutoff = Shader.PropertyToID("_Cutoff");
+
+        /// <summary>Shader.PropertyToID("_DetailAlbedoMap")</summary>
+        public static readonly int DetailAlbedoMap = Shader.PropertyToID("_DetailAlbedoMap");
+
+        /// <summary>Shader.PropertyToID("_DetailMask")</summary>
+        public static readonly int DetailMask = Shader.PropertyToID("_DetailMask");
+
+        /// <summary>Shader.PropertyToID("_DetailNormalMap")</summary>
+        public static readonly int DetailNormalMap = Shader.PropertyToID("_DetailNormalMap");
+
+        /// <summary>Shader.PropertyToID("_DetailNormalMapScale")</summary>
+        public static readonly int DetailNormalMapScale = Shader.PropertyToID("_DetailNormalMapScale");
+
         /// <summary>Shader.PropertyToID("_DstBlend")</summary>
         public static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
+
+        /// <summary>Shader.PropertyToID("_EmissionColor")</summary>
+        public static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
+
+        /// <summary>Shader.PropertyToID("_EmissionMap")</summary>
+        public static readonly int EmissionMap = Shader.PropertyToID("_EmissionMap");
 
         /// <summary>Shader.PropertyToID("_Exposure")</summary>
         public static readonly int Exposure = Shader.PropertyToID("_Exposure");
@@ -19,20 +49,56 @@ namespace Zigurous.Graphics
         /// <summary>Shader.PropertyToID("_Glossiness")</summary>
         public static readonly int Glossiness = Shader.PropertyToID("_Glossiness");
 
+        /// <summary>Shader.PropertyToID("_GlossMapScale")</summary>
+        public static readonly int GlossMapScale = Shader.PropertyToID("_GlossMapScale");
+
+        /// <summary>Shader.PropertyToID("_GlossyReflections")</summary>
+        public static readonly int GlossyReflections = Shader.PropertyToID("_GlossyReflections");
+
         /// <summary>Shader.PropertyToID("_MainTex")</summary>
         public static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
         /// <summary>Shader.PropertyToID("_Metallic")</summary>
         public static readonly int Metallic = Shader.PropertyToID("_Metallic");
 
+        /// <summary>Shader.PropertyToID("_MetallicGlossMap")</summary>
+        public static readonly int MetallicGlossMap = Shader.PropertyToID("_MetallicGlossMap");
+
         /// <summary>Shader.PropertyToID("_Mode")</summary>
         public static readonly int Mode = Shader.PropertyToID("_Mode");
+
+        /// <summary>Shader.PropertyToID("_OcclusionMap")</summary>
+        public static readonly int OcclusionMap = Shader.PropertyToID("_OcclusionMap");
+
+        /// <summary>Shader.PropertyToID("_OcclusionStrength")</summary>
+        public static readonly int OcclusionStrength = Shader.PropertyToID("_OcclusionStrength");
+
+        /// <summary>Shader.PropertyToID("_Parallax")</summary>
+        public static readonly int Parallax = Shader.PropertyToID("_Parallax");
+
+        /// <summary>Shader.PropertyToID("_ParallaxMap")</summary>
+        public static readonly int ParallaxMap = Shader.PropertyToID("_ParallaxMap");
 
         /// <summary>Shader.PropertyToID("_Ramp")</summary>
         public static readonly int Ramp = Shader.PropertyToID("_Ramp");
 
+        /// <summary>Shader.PropertyToID("_SmoothnessTextureChannel")</summary>
+        public static readonly int SmoothnessTextureChannel = Shader.PropertyToID("_SmoothnessTextureChannel");
+
+        /// <summary>Shader.PropertyToID("_SpecColor")</summary>
+        public static readonly int SpecularColor = Shader.PropertyToID("_SpecColor");
+
+        /// <summary>Shader.PropertyToID("_SpecGlossMap")</summary>
+        public static readonly int SpecularGlossMap = Shader.PropertyToID("_SpecGlossMap");
+
+        /// <summary>Shader.PropertyToID("_SpecularHighlights")</summary>
+        public static readonly int SpecularHighlights = Shader.PropertyToID("_SpecularHighlights");
+
         /// <summary>Shader.PropertyToID("_SrcBlend")</summary>
         public static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
+
+        /// <summary>Shader.PropertyToID("_UVSec")</summary>
+        public static readonly int UVSec = Shader.PropertyToID("_UVSec");
 
         /// <summary>Shader.PropertyToID("_ZWrite")</summary>
         public static readonly int ZWrite = Shader.PropertyToID("_ZWrite");
