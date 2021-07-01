602d9abc918f882bd5d16d3e1fbe540687b556fe
Adam Graham <adam@zigurous.com>
2021-07-01 04:14:50 -0500

    Add ShaderProperty to automatically generate ids from names

---
+++ Editor/ShaderPropertyPropertyDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Zigurous.Graphics.Editor
+{
+    [CustomPropertyDrawer(typeof(ShaderProperty))]
+    public sealed class ShaderPropertyPropertyDrawer : PropertyDrawer
+    {
+        private SerializedProperty _name;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (_name == null) {
+                _name = property.FindPropertyRelative("_name");
+            }
+
+            EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            _name.stringValue = EditorGUI.TextField(position, _name.stringValue);
+            EditorGUI.EndProperty();
+        }
+
+    }
+
+}
---
+++ Runtime/ShaderProperty.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Graphics
+{
+    /// <summary>
+    /// A shader property that can be set on a material. An id is automatically
+    /// created for the property for optimal code.
+    /// </summary>
+    [System.Serializable]
+    public struct ShaderProperty
+    {
+        [SerializeField]
+        [HideInInspector]
+        private int _id;
+
+        /// <summary>
+        /// The id of the shader property.
+        /// </summary>
+        public int id
+        {
+            get
+            {
+                if (_id == 0) {
+                    _id = Shader.PropertyToID(_name);
+                }
+                return _id;
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("The name of the shader property.")]
+        private string _name;
+
+        /// <summary>
+        /// The name of the shader property.
+        /// </summary>
+        public string name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                _id = Shader.PropertyToID(value);
+            }
+        }
+
+        /// <summary>
+        /// Constructs a new shader property with the given name.
+        /// </summary>
+        public ShaderProperty(string name)
+        {
+            _name = name;
+            _id = Shader.PropertyToID(name);
+        }
+
+        public static implicit operator ShaderProperty(string name) => new ShaderProperty(name);
+
+    }
+
+}
