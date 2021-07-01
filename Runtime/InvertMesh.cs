a165040c4ddbb854f370c9d84bac353e7e0d63f4
Adam Graham <adam@zigurous.com>
2021-07-01 07:54:04 -0500

    Add script to invert mesh

---
+++ Runtime/InvertMesh.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Graphics
+{
+    /// <summary>
+    /// Inverts the normals and triangles of the mesh so it renders inside-out.
+    /// </summary>
+    [RequireComponent(typeof(MeshFilter))]
+    [AddComponentMenu("Zigurous/Graphics/Invert Mesh")]
+    public sealed class InvertMesh : MonoBehaviour
+    {
+        /// <summary>
+        /// Whether the mesh is currently inverted.
+        /// </summary>
+        public bool inverted { get; private set; }
+
+        private void OnEnable()
+        {
+            if (!this.inverted) {
+                Invert();
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (this.inverted) {
+                Invert();
+            }
+        }
+
+        public void Invert()
+        {
+            this.inverted = !this.inverted;
+
+            MeshFilter filter = GetComponent<MeshFilter>();
+
+            if (filter.mesh != null)
+            {
+                filter.mesh.InvertTriangles();
+                filter.mesh.InvertNormals();
+            }
+        }
+
+    }
+
+}
